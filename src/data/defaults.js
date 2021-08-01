ea89fa1d9111d0b1e9de0f349a1b8f75fcbe9f96
ololuki <ololuki@gmail.com>
2021-08-02 00:38:23 +0200

    Disable scroll on ctrl+left mouse button - it is for incremental selection. Change default speed to match Firefox Autoscroll.

--- src/data/defaults.js
+++ src/data/defaults.js
@@ -3,13 +3,13 @@
 var defaults = {
   dragThreshold: 10,
   moveThreshold: 10,
-  moveSpeed: 10,
+  moveSpeed: 4,
   stickyScroll: true,
   innerScroll: true,
   scrollOnLinks: false,
   sameSpeed: false,
   capSpeed: "",
   shouldCap: false,
-  ctrlClick: true,
+  ctrlClick: false,
   middleClick: true
 }
